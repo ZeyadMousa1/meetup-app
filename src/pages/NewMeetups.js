c72b94d98e17373321afae055f81e1dd81c3c75b
ZeyadMousa1 <zeyadmousa1919@gmail.com>
2024-01-01 09:26:30 +0200

    connect with firebase server and add new meetup with post method

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,8 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import AddNewMeetupForm from "../components/meetup/AddNewMeetupForm";
 
 function NewMeetupPage() {
+    const navigate = useNavigate()
     function onAddMeetupHandler(meetupData) {
-
+        fetch('https://meetups-app-a25df-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/')
+        })
     }
     return (
         <section>
